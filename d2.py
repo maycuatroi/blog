3a8f1b9217896bf71ff0f58d365a666ded12aae0
Binh Nguyen <sometimesocrazy@gmail.com>
2024-10-12 17:16:53 +0700

    - update design

--- d2.py
+++ d2.py
@@ -23,20 +23,25 @@ def process_svg_file(path: str):
 
 @click.command()
 @click.argument("path", type=click.Path(exists=True))
-def build_d2(path: str):
+@click.option("-w", "--watch", is_flag=True, help="Watch on browser")
+def build_d2(path: str, watch: bool):
     """
     This script builds the D2 project and removes the 'UNLICENSED COPY' text from the output SVG.
     """
     path_output = path.replace(".d2", ".svg")
-    command = f'd2 "{path}" "{path_output}" --theme 1 --layout tala'
+    if watch:
+        command = f'd2 -w "{path}" "{path_output}" --theme 1 --layout tala --scale 1'
+    else:
+        command = f'd2 "{path}" "{path_output}" --theme 1 --layout tala --scale 1'
 
     try:
         os.system(command)
         click.echo("D2 project built successfully.")
 
         # Process the output SVG to remove the unwanted text
-        process_svg_file(path_output)
-        click.echo("SVG processed: 'UNLICENSED COPY' text removed.")
+        if not watch:
+            process_svg_file(path_output)
+            click.echo("SVG processed: 'UNLICENSED COPY' text removed.")
     except Exception as e:
         click.echo(f"An error occurred: {e}")
 
